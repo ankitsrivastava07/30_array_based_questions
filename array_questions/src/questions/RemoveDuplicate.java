0c717b9f17cf9261570e6569317013c7a1f005b4
Ankit Srivastava <ankit.srivastava7@ibm.com>
2025-03-16 04:45:06 +0530

    code changes

--- array_questions/src/questions/RemoveDuplicate.java
+++ array_questions/src/questions/RemoveDuplicate.java
@@ -1,4 +1,27 @@
 package questions;
 
+import java.util.Arrays;
+
 public class RemoveDuplicate {
+
+    public static int removeDuplicateFromArray(int[] arr) {
+        int j = 1;
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] != arr[i + 1]) {
+                arr[i] = arr[i + 1];
+                j++;
+            }
+        }
+        return j;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1, 1, 2, 3, 4, 5};
+        // System.out.println(Arrays.toString(arr));
+        int uniqueElm = removeDuplicateFromArray(arr);
+
+        for (int i = 0; i < uniqueElm; i++) {
+            System.out.println(arr[i]);
+        }
+    }
 }
